1255a1fc20ff23a083d7e869fa810b987119ed67
Keith Stevens <fozziethebeat@gmail.com>
2022-06-08 14:52:21 +0900

    Creating a simple drop down for user profile navigation

--- web/src/components/Navigation/Navigation.js
+++ web/src/components/Navigation/Navigation.js
@@ -1,24 +1,67 @@
+import md5 from 'md5'
 import { Trans } from 'react-i18next'
 import { Link, navigate, routes } from '@redwoodjs/router'
 import { useAuth } from '@redwoodjs/auth'
 import { Navbar, Nav, Dropdown } from 'rsuite'
+import styled from 'styled-components'
+
+const StyledProfile = styled.img`
+  border-radius: 50%;
+`
+
+const ProfileButton = styled.button`
+  background-color: transparent;
+  border-radius: 50%;
+`
 
 const RedwoodLink = React.forwardRef((props) => {
   return <Link {...props} />
 })
 
+function getGravatarURL(email) {
+  const address = String(email).trim().toLowerCase()
+  const hash = md5(address)
+  return `https://www.gravatar.com/avatar/${hash}`
+}
+
+const renderProfileIcon = (props, ref) => {
+  const { user, ...rest } = props
+  console.log(user)
+  const image = user.image ? user.image : getGravatarURL(user.email)
+  return (
+    <ProfileButton {...rest} ref={ref}>
+      <StyledProfile alt="Your Profile" src={image} height="32" width="32" />
+    </ProfileButton>
+  )
+}
+
 const UserItem = () => {
-  const { isAuthenticated, logOut } = useAuth()
+  const { isAuthenticated, currentUser, hasRole, logOut } = useAuth()
   if (isAuthenticated) {
     return (
-      <Nav.Item
-        onSelect={async () => {
-          await logOut()
-          navigate(routes.home())
-        }}
+      <Dropdown
+        placement="bottomEnd"
+        title="Account"
+        renderToggle={renderProfileIcon}
+        user={currentUser}
       >
-        <Trans i18nKey="auth.signOut">Sign Out</Trans>
-      </Nav.Item>
+        <Dropdown.Item as={RedwoodLink} to={routes.profileInfo()}>
+          <Trans i18nKey="layouts.profile">Profile</Trans>
+        </Dropdown.Item>
+        {hasRole('admin') && (
+          <Dropdown.Item as={RedwoodLink} to={routes.adminManageUsers()}>
+            <Trans i18nKey="layouts.admin">Admin</Trans>
+          </Dropdown.Item>
+        )}
+        <Dropdown.Item
+          onSelect={async () => {
+            await logOut()
+            navigate(routes.home())
+          }}
+        >
+          <Trans i18nKey="auth.signOut">Sign Out</Trans>
+        </Dropdown.Item>
+      </Dropdown>
     )
   }
   return (
