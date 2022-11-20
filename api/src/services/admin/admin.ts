af4bc4bd09ce90fd9b4d4b5230e1e265f44f0341
Keith Stevens <fozziethebeat@gmail.com>
2022-11-20 11:16:22 +0900

    Finishing the change out to db auth

--- api/src/services/admin/admin.ts
+++ api/src/services/admin/admin.ts
@@ -1,27 +1,18 @@
 import { db } from 'src/lib/db'
-import { supabaseClient } from 'src/lib/supabase'
 
 export const users = async () => {
-  const { data, error } = await supabaseClient.auth.api.listUsers()
-  const result = await db.userRoles.findMany()
-  const roleMap = result.reduce((m, entry) => {
-    m.set(entry.id, entry.role)
-    return m
-  }, new Map())
-  const users = data.map((user) => {
-    return {
-      id: user.id,
-      email: user.email,
-      role: roleMap.get(user.id) || 'general',
-    }
+  const users = await db.users.findMany({
+    select: {
+      id: true,
+      email: true,
+      role: true,
+    },
   })
   return users
 }
 
 export const updateUserRole = async ({ id, role }) => {
-  console.log(id)
-  console.log(role)
-  return await db.userRoles.update({
+  return await db.user.update({
     data: {
       role: role,
     },
--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,5 @@
 import { AuthProvider } from '@redwoodjs/auth'
 
-
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
@@ -11,16 +10,10 @@ import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
 import './scaffold.css'
-import 'rsuite/dist/rsuite.min.css'
 import './index.css'
 import './i18n'
 
 const extendedTheme = extendTheme(theme)
-
-const supabaseClient = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
-)
 
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
